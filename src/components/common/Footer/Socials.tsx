02145f384856733924c179ea841061e03b313ee6
ihor-soloviov <igor.musson.55@gmail.com>
2025-09-24 16:34:10 +0300

    added social media icons for different color themes and refactored footer component to use new Socials component

---
+++ src/components/common/Footer/Socials.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Plane } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+
+const socials = [
+  {
+    name: "instagram",
+    href: "https://www.instagram.com/kk_nau/",
+    iconPath: {
+      light: "/instagram-light.png",
+      dark: "/instagram-dark.png",
+    },
+  },
+  {
+    name: "facebook",
+    href: "https://www.facebook.com/kknauofficial/?rf=1794013074244809",
+    iconPath: {
+      light: "/facebook-light.png",
+      dark: "/facebook-dark.png",
+    },
+  },
+  {
+    name: "youtube",
+    href: "https://www.youtube.com/channel/UCLWVYkhjvjMsyAwofwPY_zQ",
+    iconPath: {
+      light: "/youtube-light.png",
+      dark: "/youtube-dark.png",
+    },
+  },
+  {
+    name: "telegram",
+    href: "https://t.me/kk_nau",
+    iconPath: {
+      light: "/telegram-light.png",
+      dark: "/telegram-dark.png",
+    },
+  },
+];
+
+export const Socials = () => {
+  const { theme } = useTheme();
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        <Plane className="h-6 w-6 text-blue-600" />
+        <span className="text-xl font-bold">Авіаційний коледж</span>
+      </div>
+      <p className="mt-4 text-sm text-muted-foreground">
+        Надаємо освіту в галузі авіації з 1951 року.
+      </p>
+      <div className="mt-4 flex gap-4">
+        {socials.map((social) => (
+          <Link
+            key={social.name}
+            href={social.href}
+            className="text-muted-foreground hover:text-blue-600"
+          >
+            <span className="sr-only">{social.name}</span>
+            {theme === "dark" ? (
+              <Image
+                src={social.iconPath.dark}
+                alt={social.name}
+                width={24}
+                height={24}
+              />
+            ) : (
+              <Image
+                src={social.iconPath.light}
+                alt={social.name}
+                width={24}
+                height={24}
+              />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,138 +1,110 @@
-import Link from 'next/link'
-import { Plane } from 'lucide-react'
-import React from 'react'
-import Image from 'next/image'
-import { links } from '@/lib/navigation'
-
-const socials = [
-    {
-        name: 'instagram',
-        href: 'https://www.instagram.com/kk_nau/',
-        iconPath: '/instagram.svg'
-    },
-    {
-        name: 'facebook',
-        href: 'https://www.facebook.com/kknauofficial/?rf=1794013074244809',
-        iconPath: '/facebook.svg'
-    },
-
-    {
-        name: 'youtube',
-        href: 'https://www.youtube.com/channel/UCLWVYkhjvjMsyAwofwPY_zQ',
-        iconPath: '/youtube.svg'
-    },
-    {
-        name: 'telegram',
-        href: 'https://t.me/kk_nau',
-        iconPath: '/telegram.svg'
-    }
-]
+import Link from "next/link";
+import React from "react";
+import { links } from "@/lib/navigation";
+import { Socials } from "../common/Footer/Socials";
 
 const Footer = () => {
-    return (
-        <footer className="border-t bg-gray-50 dark:bg-blue-900/10">
-            <div className="container py-12 mx-auto">
-                <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-                    <div>
-                        <div className="flex items-center gap-2">
-                            <Plane className="h-6 w-6 text-blue-600" />
-                            <span className="text-xl font-bold">Авіаційний коледж</span>
-                        </div>
-                        <p className="mt-4 text-sm text-muted-foreground">
-                            Надаємо освіту в галузі авіації з 1951 року.
-                        </p>
-                        <div className="mt-4 flex gap-4">
-                            {socials.map((social) => (
-                                <Link key={social.name} href={social.href} className="text-muted-foreground hover:text-blue-600">
-                                    <span className="sr-only">{social.name}</span>
-                                    <Image src={social.iconPath} alt={social.name} width={24} height={24} className="fill-white" />
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
+  return (
+    <footer className="border-t bg-gray-50 dark:bg-blue-900/10">
+      <div className="container py-12 mx-auto">
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <Socials />
 
-                    <div>
-                        <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">Навігація</h4>
-                        <ul className="space-y-2 text-sm">
-                            {links.map((item) => (
-                                <li key={item.label}>
-                                    <Link href={item.href} className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600">
-                                        {item.label}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+          <div>
+            <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">
+              Навігація
+            </h4>
+            <ul className="space-y-2 text-sm">
+              {links.map((item) => (
+                <li key={item.label}>
+                  <Link
+                    href={item.href}
+                    className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                    <div>
-                        <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">Ресурси</h4>
-                        <ul className="space-y-2 text-sm">
-                            {["Admissions", "Financial Aid", "Career Services", "Student Life", "Alumni Network"].map((item) => (
-                                <li key={item}>
-                                    <Link href="#" className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600">
-                                        {item}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+          <div>
+            <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">
+              Ресурси
+            </h4>
+            <ul className="space-y-2 text-sm">
+              {[
+                "Admissions",
+                "Financial Aid",
+                "Career Services",
+                "Student Life",
+                "Alumni Network",
+              ].map((item) => (
+                <li key={item}>
+                  <Link
+                    href="#"
+                    className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+                  >
+                    {item}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                    <div>
-                        <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">Контакти</h4>
-                        <address className="not-italic">
-                            <a
-                                href="https://maps.app.goo.gl/UzDMeywUZNE1dkKv5"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
-                            >
-                                вулиця Олега Антонова, 1
-                            </a>
-                            <br />
-                            <a
-                                href="https://maps.app.goo.gl/UzDMeywUZNE1dkKv5"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
-                            >
-                                Кривий Ріг, 50024
-                            </a>
-                            <br />
-                            <a
-                                href="mailto:pochta@krfk.kai.edu.ua"
-                                className="mt-2 text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
-                            >
-                                pochta@krfk.kai.edu.ua
-                            </a>
-                            <br />
-                            <a
-                                href="tel:+380678241414"
-                                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
-                            >
-                                067-824-14-14
-                            </a>
+          <div>
+            <h4 className="mb-4 text-sm font-semibold uppercase tracking-wider">
+              Контакти
+            </h4>
+            <address className="not-italic">
+              <a
+                href="https://maps.app.goo.gl/UzDMeywUZNE1dkKv5"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+              >
+                вулиця Олега Антонова, 1
+              </a>
+              <br />
+              <a
+                href="https://maps.app.goo.gl/UzDMeywUZNE1dkKv5"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+              >
+                Кривий Ріг, 50024
+              </a>
+              <br />
+              <a
+                href="mailto:pochta@krfk.kai.edu.ua"
+                className="mt-2 text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+              >
+                pochta@krfk.kai.edu.ua
+              </a>
+              <br />
+              <a
+                href="tel:+380678241414"
+                className="text-sm text-muted-foreground transition-colors duration-300 hover:text-blue-600"
+              >
+                067-824-14-14
+              </a>
+            </address>
+          </div>
+        </div>
 
-                        </address>
-                    </div>
-                </div>
+        <div className="mt-12 border-t pt-8 text-center">
+          <div className="text-xs text-muted-foreground">
+            <span className="font-bold">&copy; {new Date().getFullYear()}</span>{" "}
+            <span>
+              Криворізький фаховий коледж державного некомерційного підприємства
+              «Державний університет «Київський авіаційний інститут»
+            </span>
+            <span />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-                <div className="mt-12 border-t pt-8 text-center">
-                    <div className="text-xs text-muted-foreground">
-                        <span className="font-bold">
-                            &copy; {new Date().getFullYear()}
-                        </span>
-                        {" "}
-                        <span>
-                            Криворізький фаховий коледж
-                            державного некомерційного підприємства
-                            «Державний університет «Київський авіаційний інститут»
-                        </span>
-                        <span />
-                    </div>
-                </div>
-            </div>
-        </footer >
-    )
-}
-
-export default Footer
+export default Footer;
